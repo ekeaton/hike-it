0e002cfc3aed7ebc1db4412ce5ab01701a1e1bc5
Estelle K <e.keaton@live.com>
2019-03-31 18:45:31 -0700

    updated hike.js

--- hike.js
+++ hike.js
@@ -36,7 +36,7 @@ function displayResults(responseJson){
   $('#results').removeClass('hidden');
   document.querySelector('#results').scrollIntoView({ 
     behavior: 'smooth' 
-    )};
+    });
   };
 
 // using the responseJson results from googlemaps api searchForTrails takes the lat and lon to pass in as values to retrieve the data
